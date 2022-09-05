7e442e0925509625f6a0cd7e7da1b129e1e26b54
pratap86 <narayanpratap86@gmail.com>
2022-09-05 17:56:20 +0530

    initial commit

---
+++ src/main/java/com/pratap/configurator/ConfiguratorApplication.java
@@ -0,0 +1,15 @@
+package com.pratap.configurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguratorApplication.class, args);
+	}
+
+}
